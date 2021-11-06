6368f771b05f76a1534548bfe6ba3b0b3e08876d
gustavo-nt <gustavont.dev@gmail.com>
2021-11-06 16:08:49 -0300

    Remove comments on functions

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,7 +15,6 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
     if (newTaskTitle.length) {
       const newTask = {
         id: Math.floor(Math.random() * 1000),
@@ -28,7 +27,6 @@ export function TaskList() {
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID]
     const selected = tasks.find(task => task.id === id);
     const selectedIndex = tasks.findIndex(task => task.id === id);
     
@@ -40,7 +38,6 @@ export function TaskList() {
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
     setTasks((tasks.filter(task => task.id !== id)));
   }
 
